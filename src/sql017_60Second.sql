7b6e5a8aef418154c962b9b1767c95ba112e304e
563909408@qq.com <563909408@qq.com>
2018-10-30 10:59:23 +0800

    2018_10_30   weekday(2)   10:59:18

--- src/sql017_60Second.sql
+++ src/sql017_60Second.sql
@@ -1,1 +1,7 @@
-update prdt set chk_bat='T' where idx1 like '33%' and chk_bat='F'
+-- 以前的
+-- update prdt set chk_bat='T' where idx1 like '33%' and chk_bat='F'
+
+
+-- 2018_10_30   weekday(2)   10:42:50  现在的
+
+update prdt set chk_bat='T' where idx1 like '33%' and (chk_bat='F' or chk_bat IS NULL)
